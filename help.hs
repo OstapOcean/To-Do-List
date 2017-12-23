d7e0d8ef15b6892a8391acae062d1b113550133d
OstapOcean <Ostap@Mac-Ostap.local>
2017-12-23 19:45:56 +0200

    Fixed help description

--- help.hs
+++ help.hs
@@ -9,7 +9,7 @@ import Common
 
 cmdHelp :: [String] -> IO ()
 cmdHelp param = do
-                putStrLn (limiter ++ "To execute command enter or command's number or name (in square brackets).")
-                putStrLn (limiter ++ "After command identifier enter parametres.")
-                putStrLn (limiter ++ "As params enter point number or new value.")
-                putStrLn (limiter ++ "For show and delete commands you can enter param \"all\".")
+                putStrLn (limiter ++ "To execute command enter command's number or name (in square brackets).")
+                putStrLn (limiter ++ "After command identifier enter parameters.")
+                putStrLn (limiter ++ "As parameters enter point number or new value.")
+                putStrLn (limiter ++ "For show and delete commands you can enter parameter \"all\".")
